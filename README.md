ae503f94d8300d01cdfa8c7746a250a3fe44ef6c
wulfebw <wulfebw@stanford.edu>
2018-02-03 12:46:25 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# Description
+- this repo contains code associated with the robotics / rl project ["Building a robotic arm with visual perception and learning to control it with reinforcement and imitation learning"](https://wulfebw.github.io/blog/rl/robotics/robotics-intro)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='rlrobo',
+      version='0.1',
+      description='Robotics / RL project code',
+      author='Blake Wulfe',
+      author_email='blake.w.wulfe@gmail.com',
+      license='MIT',
+      packages=['rlrobo'],
+      zip_safe=False,
+      install_requires=[
+        'numpy'
+      ])
