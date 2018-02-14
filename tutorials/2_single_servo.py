d0745140888abe5ace06809231c93bf9184eaf68
blake wulfe <blake.w.wulfe@gmail.com>
2018-02-14 05:03:24 +0000

    updating tutorial 2 to use rlrobo utils

--- tutorials/2_single_servo.py
+++ tutorials/2_single_servo.py
@@ -4,33 +4,13 @@ import time
 
 from Adafruit_PWM_Servo_Driver import PWM
 
-def set_servo_angle(
-        pwm,
-        channel,
-        angle,
-        min_angle=10,
-        max_angle=170,
-        min_pulse=150,
-        max_pulse=600):
-  '''
-  Description:
-      - set servo angle
-      - max_angle corresponds to maximum position clockwise with servo facing upward
-  '''
-  # clip angle to valid range
-  angle = max(min(angle, max_angle), min_angle)
-
-  # linearly interpolate between min_pulse and max_pulse
-  # depending on where in the possible range angle falls
-  frac = (angle - min_angle) / (max_angle - min_angle)
-  pulse = int(min_pulse + frac * (max_pulse - min_pulse))
-  pwm.setPWM(channel, 0, pulse)
+from rlrobo.utils import set_servo_angle
 
 # Initialise the PWM device using the default address
 pwm = PWM(0x40)
 
-servoMin = 150  # Min pulse length out of 4096
-servoMax = 600  # Max pulse length out of 4096
+min_pulse = 150  # Min pulse length out of 4096
+max_pulse = 600  # Max pulse length out of 4096
 min_angle = 10
 max_angle = 170
 
@@ -42,9 +22,9 @@ sleep_time = .005
 while True:
 
   for angle in np.linspace(min_angle, max_angle, steps):
-    set_servo_angle(pwm, 0, angle, min_angle=min_angle, max_angle=max_angle)
+    set_servo_angle(pwm, 0, angle, min_angle=min_angle, max_angle=max_angle, min_pulse=min_pulse, max_pulse=max_pulse)
     time.sleep(sleep_time)
 
   for angle in reversed(np.linspace(min_angle, max_angle, steps)):
-    set_servo_angle(pwm, 0, angle, min_angle=min_angle, max_angle=max_angle)
+    set_servo_angle(pwm, 0, angle, min_angle=min_angle, max_angle=max_angle, min_pulse=min_pulse, max_pulse=max_pulse)
     time.sleep(sleep_time)
